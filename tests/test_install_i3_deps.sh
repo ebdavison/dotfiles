757e65832665c896e2b40a91fb005b01fd9c2ee4
Ed Davison <549431+ebdavison@users.noreply.github.com>
2026-08-26 10:05:28 -0400

    update i3 for rbw

--- tests/test_install_i3_deps.sh
+++ tests/test_install_i3_deps.sh
@@ -60,6 +60,24 @@ test_installer_has_dry_run_and_uses_fedora_package_manager() {
   assert_contains "$SCRIPT_UNDER_TEST" "sudo"
 }
 
+test_rofi_rbw_is_installed_with_pipx_not_cargo() {
+  assert_contains "$SCRIPT_UNDER_TEST" "pipx"
+  assert_contains "$SCRIPT_UNDER_TEST" "PIPX_APPS=("
+  assert_contains "$SCRIPT_UNDER_TEST" "rofi-rbw"
+
+  if awk '/CARGO_CRATES=\(/,/\)/ { print }' "$SCRIPT_UNDER_TEST" | grep -Fq "rofi-rbw"; then
+    fail "rofi-rbw is a Python/pipx app and must not be listed as a cargo crate"
+  fi
+}
+
+test_rofi_rbw_installs_newer_rbw_and_i3_prefers_user_cargo_bin() {
+  if ! awk '/CARGO_CRATES=\(/,/\)/ { print }' "$SCRIPT_UNDER_TEST" | grep -Fq "rbw"; then
+    fail "rofi-rbw 1.7 requires rbw with --raw support; install newer rbw as a cargo crate"
+  fi
+
+  assert_contains "$I3_CONFIG_UNDER_TEST" 'PATH=$HOME/.cargo/bin:$HOME/.local/bin:$PATH rofi-rbw'
+}
+
 test_i3_instant_layout_typo_is_fixed() {
   assert_not_contains "$I3_CONFIG_UNDER_TEST" "i3-isntant-layout"
   assert_contains "$I3_CONFIG_UNDER_TEST" "i3-instant-layout -"
@@ -73,6 +91,8 @@ run_test() {
 
 run_test test_installer_script_exists_and_covers_i3_dependencies
 run_test test_installer_has_dry_run_and_uses_fedora_package_manager
+run_test test_rofi_rbw_is_installed_with_pipx_not_cargo
+run_test test_rofi_rbw_installs_newer_rbw_and_i3_prefers_user_cargo_bin
 run_test test_i3_instant_layout_typo_is_fixed
 
 echo "All i3 dependency installer tests passed"
